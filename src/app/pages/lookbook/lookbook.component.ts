9396bf6fb7e088f7eaa8425d9187dcc5e64d24f4
aaron-plahn <aplahn85@gmail.com>
2020-08-19 09:02:10 -0700

    Finish wiring up basic components

--- src/app/pages/lookbook/lookbook.component.ts
+++ src/app/pages/lookbook/lookbook.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
-import { map, switchMap, filter } from 'rxjs/operators';
+import { ActivatedRoute, Router } from "@angular/router";
+import { map, switchMap } from 'rxjs/operators';
 
 import { LookbookService } from '../../services/lookbook.service';
 import { Lookbook } from '../../types/types';
@@ -16,18 +16,23 @@ export class LookbookComponent implements OnInit {
   // lookbookID: string = "2";
   lookbook: Lookbook;
 
-  constructor(private lookbookService: LookbookService, private route: ActivatedRoute) {
+  constructor(private lookbookService: LookbookService, private route: ActivatedRoute, private router: Router) {
     this.route.params
      .pipe(map(params=>params['id']))
      .pipe(switchMap((id:string):ObservableInput<any>=>{
        return this.lookbookService.getLookbookByID(id);
      }))
      .subscribe((data:Lookbook)=>{
+       if(!data) return this.navigateToMaster();
        this.lookbook = data;
-     })
+     });
    }
 
   ngOnInit(): void {
+  }
+
+  private navigateToMaster(): void{
+       this.router.navigate(['/lookbooks']);
   }
 
 }
