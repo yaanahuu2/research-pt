7730e3c35a7110bf09d9fa7739398bf546c9b586
aaron-plahn <aplahn85@gmail.com>
2020-08-26 15:28:39 -0700

    Implement Angular Material table

--- src/app/pages/lookbooks/lookbooks.component.ts
+++ src/app/pages/lookbooks/lookbooks.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { LookbookService } from '../../services/lookbook.service';
 import { Lookbook } from '../../types/types';
+import {MatPaginator} from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-lookbooks',
@@ -8,16 +10,21 @@ import { Lookbook } from '../../types/types';
   styleUrls: ['./lookbooks.component.css']
 })
 export class LookbooksComponent implements OnInit {
-  lookbooks: Lookbook[];
+  lookbooks: MatTableDataSource<Lookbook> = new MatTableDataSource<Lookbook>();
+  displayedColumns: string[] = ["id","name","author"];
   mockPageLoadTimeout: number = 1500;
   messageFromLoader: string = "SUCCESS";
   loaded: boolean = false;
 
   constructor( private lookbookService: LookbookService ) { }
 
+  
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
   ngOnInit(): void {
+    this.lookbooks.paginator = this.paginator;
     this.lookbookService.getLookbooks().subscribe((data: Lookbook[])=>{
-      this.lookbooks = data;
+      this.lookbooks = new MatTableDataSource(data);
     });
     this.lookbookService.mockPageLoad(this.mockPageLoadTimeout).subscribe((response: boolean)=>{
       this.loaded = response;
